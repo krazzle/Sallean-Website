3c9cc872c03be5151311975ca46bbf5596aaf226
Alyssa Sallean <krazzle@cs.utexas.edu>
2013-11-18 17:47:33 -0600

    fixed nav tabs?

--- php-website/includes/nav.php
+++ php-website/includes/nav.php
@@ -1,7 +1,9 @@
 <div id="nav">
-	<a href="About.php">About</a>
-	<a href="Projects.php">Projects</a>
-	<a href="Resume.php">Resume</a>
-	<a href="Fun.php">For Fun!</a>
+<ul>
+	<li><a href="About.php">About</a></li>
+	<li><a href="Projects.php">Projects</a></li>
+	<li><a href="Resume.php">Resume</a></li>
+	<li><a href="Fun.php">For Fun!</a></li>
+</ul>
 </div> <!-- end nav  -->
 
