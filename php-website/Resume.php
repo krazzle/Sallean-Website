7ee075c2b19b3d7f064dff4d7fabc80e716d92c2
Alyssa Sallean <krazzle@cs.utexas.edu>
2013-11-18 15:39:34 -0600

    yeah

---
+++ php-website/Resume.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC -"//W3C//DTD XHTML 1.0 Strict//EN" "http://http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+<title>1stWebDesigner PHP Tempalate</title>
+</head>
+
+<body>
+	<div id="wrapper">
+		<?php include('includes/header.php'); ?>
+		<?php include('includes/nav.php'); ?>
+		<div id="content">
+			<p> include Resume here </p>
+		</div> <!-- end content -->
+		<?php include('includes/footer.php'); ?>
+	</div> <!-- end wrapper -->
+</body>
+</html>
+
+
