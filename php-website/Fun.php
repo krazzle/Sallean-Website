63c83bc28c84d75a85917f0674ea49194b316f86
Alyssa Sallean <krazzle@cs.utexas.edu>
2013-11-18 16:23:53 -0600

    whooot sty le sheet updated

--- php-website/Fun.php
+++ php-website/Fun.php
@@ -7,11 +7,13 @@
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
-<title>1stWebDesigner PHP Tempalate</title>
+<?php inlude('includes/fonts.php'); ?>
+<title>AlyssaSallean</title>
 </head>
 
 <body>
 	<div id="wrapper">
+		<?php include('includes/header.php'); ?>
 		<?php include('includes/nav.php'); ?>
 		<div id="content">
 			<p> include fun stuff about hobbies here </p>
